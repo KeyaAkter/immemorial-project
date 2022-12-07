ab1d2c988ae1245b13da1ab326373e396de05b5d
KeyaAkter <sanhatanvin12@gmail.com>
2022-12-07 23:50:23 +0600

    project initialized by adding navbar

---
+++ src/components/Hero.js
@@ -0,0 +1,14 @@
+const Hero = () => {
+  return (
+    <section className="hero wrapper">
+      <h1 className="ethereal">
+        Ethereal <span></span>
+      </h1>
+      <h1 className="canvas">
+        Canvas <span></span>
+      </h1>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar wrapper">
+      <ul className="links">
+        <li>
+          <Link to="/featured">Featured</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/gallery">Gallery</Link>
+        </li>
+      </ul>
+      <div className="logo">
+        <Link to="/">
+          <h2>Immemorial</h2>
+        </Link>
+      </div>
+      <div className="favourite-link">
+        <Link to="/favourites">Favourites</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
